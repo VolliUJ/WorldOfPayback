e99f929a952065205cae9648d921976e9f382d91
Marcin Golli <marcin.golli@itmagination.com>
2023-05-15 09:19:28 +0200

    Initial Commit

---
+++ WorldOfPayback/WorldOfPaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPaybackApp.swift
+//  WorldOfPayback
+//
+//  Created by Marcin Golli on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
